587131ec396f4920cc1166102cf3e37767a9c7d3
Bartek Kisielewiczgit config --global user.name Bartek <bartkub5574@gmail.com>
2018-04-28 12:15:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title> Warriors - Browser Game </title>
+</head>
+<body>
+
+	“The supreme art of war is to subdue the enemy without fighting.” 
+	― Sun Tzu, The Art of War <br><br>
+
+	<form>
+		
+		Login:<br>
+		<input type="text" name="login"> <br>
+		Password:<br>
+		<input type="password" name="password"> <br>
+		<input type="submit" name="Login">
+
+	</form>
+
+</body>
+</html>
